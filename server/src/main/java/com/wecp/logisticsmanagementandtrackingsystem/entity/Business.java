bb58685ad975c2316654cd8571289aaa1585d579
username <wecp-dummy@gmail.com>
2025-02-08 08:36:07 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/logisticsmanagementandtrackingsystem/entity/Business.java
@@ -0,0 +1,68 @@
+package com.wecp.logisticsmanagementandtrackingsystem.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "business", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Cargo> cargos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Cargo> getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(List<Cargo> cargos) {
+        this.cargos = cargos;
+    }
+
+    public Business() {
+    }
+
+    public Business(Long id, String name, String email, List<Cargo> cargos) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cargos = cargos;
+    }
+
+    
+
+
+}
---
+++ server/src/main/java/com/wecp/logisticsmanagementandtrackingsystem/entity/Cargo.java
@@ -0,0 +1,80 @@
+package com.wecp.logisticsmanagementandtrackingsystem.entity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Cargo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String content;
+    String size;
+    String status;
+
+    @ManyToOne
+    //@JoinColumn(name="business_id")
+    @JsonBackReference
+    private Business business;
+
+    @ManyToOne
+    //@JoinColumn(name="business_id")
+    @JsonBackReference
+    private Driver driver;
+
+    public Cargo(Long id, String content, String size, String status) {
+        this.id = id;
+        this.content = content;
+        this.size = size;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Business getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(Business business) {
+        this.business = business;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
