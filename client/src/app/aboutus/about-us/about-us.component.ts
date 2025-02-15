65284055a76d5f61d0492b372d4cfe47a89630bd
username <wecp-dummy@gmail.com>
2025-02-15 19:14:06 +0000

    wecp initial commit

--- client/src/app/aboutus/about-us/about-us.component.ts
+++ client/src/app/aboutus/about-us/about-us.component.ts
@@ -5,11 +5,48 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent {
+  activeIndex = 0;
 
-  constructor() { }
 
-  ngOnInit(): void {
+teamMembers = [
+    {
+      name: 'Divyajyoti Singhdeo',
+      role: 'Team Leader',
+      image: 'proxy/3000/../../assets/testimonial1.jpeg',
+      quote: 'Leadership is not about being in charge, it’s about taking care of those in your charge.'
+    },
+    {
+      name: 'Aditi Sharma',
+      role: 'Team Member',
+      image: 'assets/member1.jpg',
+      quote: 'Code is like humor. When you have to explain it, it’s bad.'
+    },
+    {
+      name: 'Ankit Jha',
+      role: 'Team Member',
+      image: 'assets/member2.jpg',
+      quote: 'Design is not just what it looks like and feels like. Design is how it works.'
+    },
+    {
+      name: 'Devyani Ushir',
+      role: 'Team Member',
+      image: 'assets/member3.jpg',
+      quote: 'The best way to predict the future is to invent it.'
+    },
+    {
+      name: 'Aswan S',
+      role: 'Project Manager',
+      image: 'assets/member4.jpg',
+      quote: 'Good management is the art of making problems so interesting that everyone wants to solve them.'
+    }
+  ];
+
+  prevSlide() {
+    this.activeIndex = (this.activeIndex - 1 + this.teamMembers.length) % this.teamMembers.length;
   }
 
+  nextSlide() {
+    this.activeIndex = (this.activeIndex + 1) % this.teamMembers.length;
+  }
 }
